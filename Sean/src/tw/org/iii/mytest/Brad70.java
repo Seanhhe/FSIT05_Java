97eb53c97efdfd2df69a168de246876aa817f659
SEAN <SEAN@SEAN-Thinkpad>
2018-08-19 09:00:40 +0800

    20180818

---
+++ Sean/src/tw/org/iii/mytest/Brad70.java
@@ -0,0 +1,22 @@
+package tw.org.iii.mytest;
+
+public class Brad70 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		Integer b = new Integer(10);
+		Integer c = new Integer("10");
+		Integer d = 10; // auto-boxing 自動封裝
+		System.out.println(b + 4);//物件b unWrapper 解包裝 => auto-unboxing，b仍是物件。
+		System.out.println(d + 6); 
+		
+		System.out.println(a == b);
+		System.out.println(c == b); //b不同於c。兩者是物件實體在比較
+		
+		System.out.println(d == a);
+		System.out.println("--------");
+		System.out.println(b.equals(a));//此時a被自動封裝後才比較其值
+
+	}
+
+}
