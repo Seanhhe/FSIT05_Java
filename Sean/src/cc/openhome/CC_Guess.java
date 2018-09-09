75fdd848a843972fe700105c0486dc1bff934fd9
Seanhhe <seanhhe@gmail.com>
2018-09-09 23:17:50 +0800

    practice

---
+++ Sean/src/cc/openhome/CC_Guess.java
@@ -0,0 +1,12 @@
+package cc.openhome;
+
+import java.util.Scanner;
+
+public class CC_Guess {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner userInput = new Scanner(System.in);
+	}
+
+}
